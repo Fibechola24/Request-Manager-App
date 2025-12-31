5535da4df50ff5267f459befddc11ed15f344409
Fibechola24 <okpanachivictor0@gmail.com>
2025-12-31 10:46:11 +0100

    Enhanced: screen loading

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,16 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function(Controller, MessageToast) {
+    "sap/ui/core/mvc/Controller"
+], function(Controller) {
     "use strict";
 
     return Controller.extend("ui5.requestmanagerapp.controller.App", {
         onInit: function() {
-            console.log("App controller loaded");
-        },
-
-        onTestPress: function() {
-            MessageToast.show("Hello from IT Request Manager!");
+            console.log("App controller initialized");
+            
+            // Get the App control
+            var oApp = this.byId("app");
+            
+            // Remove the initial loading page after router initializes
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.attachRouteMatched(function() {
+                // Remove the initial page if it exists
+                var aPages = oApp.getPages();
+                if (aPages.length > 1) {
+                    oApp.removePage(aPages[0]);
+                }
+            });
         }
     });
 });
--- webapp/controller/Dashboard.controller.js
+++ webapp/controller/Dashboard.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
+], function(Controller, MessageBox) {
     "use strict";
 
     return Controller.extend("ui5.requestmanagerapp.controller.Dashboard", {
@@ -14,6 +15,11 @@ sap.ui.define([
 
         onNavigateToMyRequests: function() {
             this.getOwnerComponent().getRouter().navTo("myrequests");
+        },
+
+        onHelp: function() {
+            var oBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+            MessageBox.information(oBundle.getText("helpMessage"));
         }
     });
 });
